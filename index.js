3e84ebe8b21794f755848fb80d150523a30c8edd
sivernorman <silvernorman.o@gmail.com>
2021-11-15 21:41:54 +0300

    updated index.html file

--- index.js
+++ index.js
@@ -164,12 +164,12 @@ function getGender(){
       }else if(dayValue == -0){
         alert("You were born on " +dayNames[6] + " and Your akan name is " +femaleNames[6]+"!");
       }
-    break
-    default:
+      break
+      default:
+    }
   }
-}
-function findName(){
-  dayValue = calculateDayValue();
-  getGender();
-} 
- 
+  function findName(){
+    dayValue = calculateDayValue();
+    getGender();
+  } 
+   
